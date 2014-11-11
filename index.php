1fd33a0a1b80c357694860382f406126d56a746a
Marsian <mars910209@gmail.com>
2014-11-10 17:06:43 -0800

    Update index.php
    Delete redundant code.

--- index.php
+++ index.php
@@ -22,18 +22,12 @@
          include( $_GET['page'] );
          echo "</div></div>";
       }
-
-      /*if ( $_GET['keywords'] ) {
-         echo "<div class=\"container\"><div class=\"jumbotron\">";
-         include( "search.php" );
-         echo "</div></div>";
-      }*/
    ?>
    <!-- Bootstrap core JavaScript
    ================================================== -->
    <!-- Placed at the end of the document so the pages load faster -->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-	<script src="bootstrap-3.3.0-dist/dist/js/bootstrap.min.js"></script>
+   <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+   <script src="bootstrap-3.3.0-dist/dist/js/bootstrap.min.js"></script>
    </body>
 </html>
